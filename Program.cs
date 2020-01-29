29b9cfaaac685e7369913862966511c5e704b570
Jose Carlos Telheiro Junior <joset.junior@tegma.com.br>
2020-01-28 23:35:38 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ll = new LinkedList<int>(Enumerable.Range(1, 10).ToList());
+            var assassino = ll.First;
+            ExecutarMatanca(ll, assassino);
+            Console.WriteLine("Finalizado");
+            Console.ReadKey();
+
+        }
+
+        static void ExecutarMatanca(LinkedList<int> ll, LinkedListNode<int> assassino)
+        {
+            if (ll.Count > 1) 
+            { 
+                var vitima = assassino.Next ?? ll.First;
+                Console.WriteLine($"{assassino.Value} matou {vitima.Value}");
+                var novoAssassino = vitima.Next ?? ll.First;
+                ll.Remove(vitima);
+                ExecutarMatanca(ll, novoAssassino);
+            }
+            else
+            {
+                var suicida = ll.First;
+                Console.WriteLine($"{suicida.Value} suicidou-se");
+                ll.Remove(suicida);
+            }
+        }
+    }
+}
